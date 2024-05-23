1074259dd2b88f8ebf1869aad0164a1e2c832b33
Giacovelli Ciro Francesco <ciro254@gmail.com>
2024-05-23 15:52:19 +0200

    creati models per filiali e automezzi

---
+++ app/Models/Automezzo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Automezzo extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['targa', 'marca', 'modello', 'filiale_id'];
+
+    public function filiale()
+    {
+        return $this->belongsTo(Filiale::class, 'filiale_id', 'codice');
+    }
+}
---
+++ app/Models/Filiale.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Filiale extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['indirizzo', 'città', 'cap'];
+
+    public function automezzi()
+    {
+        return $this->hasMany(Automezzo::class, 'filiale_id', 'codice');
+    }
+}
