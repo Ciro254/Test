9a6eec6bb1bf51c42a1c1d12c4f2745356d4bb7d
Giacovelli Ciro Francesco <ciro254@gmail.com>
2024-05-25 10:13:06 +0200

    modifiche su views di filiali

---
+++ resources/views/Filiali/index.blade.php
@@ -0,0 +1,14 @@
+@extends('welcome')
+
+@section('content')
+    <h1>Lista delle Filiali</h1>
+    
+    <ul>
+        @foreach ($filiali as $filiale)
+            <li>
+                <a href="{{ route('filiali.show', $filiale->codice) }}">{{ $filiale->indirizzo }} - {{ $filiale->città }}</a>
+                
+            </li>
+        @endforeach
+    </ul>
+@endsection
